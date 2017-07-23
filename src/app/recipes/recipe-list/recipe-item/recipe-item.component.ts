2f6931f6ee2294a0556b434c6e4f5e5adb5e3748
GANDALF\Olek <alemik@op.pl>
2017-07-23 17:07:51 +0200

    recipe.service is done

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-import {Recipe} from '../../recipe.model';
+import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,15 +10,14 @@ import {Recipe} from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem: Recipe;
-  @Output() selectedRecipe = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
   onSelectedRecipe() {
-    this.selectedRecipe.emit();
+    this.recipeService.recipeSelected.emit(this.recipeItem);
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
+import { EventEmitter } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+  public recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe('Pierwszy przepis', 'Gotowanie pierwszego', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Drugi przepis', 'Smażenie drugiego', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Trzeci przepis', 'Pieczenie trzeciego', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  getRecipes() {
+      return this.recipes.slice();
+  }
 }
