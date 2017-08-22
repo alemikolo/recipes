ef7686af258455465e7047298ce8578b61392b4d
GANDALF\Olek <alemik@op.pl>
2017-08-22 13:44:49 +0200

    no message

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes/recipes.component';
+import {ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const = appRoutes: Routes = [
+  { path: '', }
+]
